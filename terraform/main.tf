0995f5c384ac8a7a6637593ea26975655da9bb4b
Vinay Kumar <gvkvinay6@gmail.com>
2026-05-01 14:04:39 +0530

    Added AKS Terraform config

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,3 +10,26 @@ resource "azurerm_container_registry" "acr" {
   sku                 = "Basic"
   admin_enabled       = false
 }
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "aks-multiregion-demo"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  dns_prefix          = "aksdemo"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_B2s"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_role_assignment" "acr_pull" {
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name = "AcrPull"
+  scope                = azurerm_container_registry.acr.id
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,11 @@ output "acr_login_server" {
 output "resource_group_name" {
   value = azurerm_resource_group.main.name
 }
+
+output "aks_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
+
+output "aks_resource_group" {
+  value = azurerm_resource_group.main.name
+}
