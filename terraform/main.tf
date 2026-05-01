58eca761d81f9a1746622b45f5c24c238de06dc4
Vinay Kumar <gvkvinay6@gmail.com>
2026-05-01 15:35:46 +0530

    CI V2

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,7 +20,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name       = "default"
     node_count = 1
-    vm_size    = "Standard_B2s"
+    vm_size    = "Standard_B2s_v2"
   }
 
   identity {
