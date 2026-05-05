3f17aa9aa051822646cb126f7a8bbd2dcbd50a77
vinaygvk24 <ssc2k12rgpt@gmail.com>
2026-05-05 11:20:54 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,23 @@
 from flask import Flask
 from flask_cors import CORS
 import random
+import os
 
 app = Flask(__name__)
 CORS(app)  # allow frontend to call backend
 
+# Get region from environment variable
+REGION = os.getenv("REGION", "Unknown Region")
+
 @app.route("/")
 def home():
-    return "hi vinay"
+    return f"Hello from {REGION}"
 
 @app.route("/api/random")
 def random_string():
     letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
     result = ''.join(random.choice(letters) for i in range(8))
-    return result
+    return f"{REGION}: {result}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
